7a1a71e3139922455f4ee89a12a0de684022e608
ana <adscarlos@gmail.com>
2015-10-25 19:05:05 +0000

    very basic todo list: items can be added and input field gets cleaned after submission

--- js/todoListController.js
+++ js/todoListController.js
@@ -1,3 +1,11 @@
-app.controller('todoListController', function() {
-  this.todos = [];
+app.controller('todoListController', function($scope) {
+
+  $scope.todoStore = [];
+
+  $scope.addTodo = function() {
+    $scope.todoStore.push($scope.todo);
+    $scope.todo = '';
+
+  };
+
 });
--- test/todoList.spec.js
+++ test/todoList.spec.js
@@ -2,12 +2,19 @@ describe('todoListController', function() {
   beforeEach(module('todoList'));
 
   var ctrl;
+  var $scope;
 
   beforeEach(inject(function($controller) {
-    ctrl = $controller('todoListController');
+    $scope = {};
+    ctrl = $controller('todoListController', {$scope: $scope});
   }));
 
   it('initialises with an empty todo list', function() {
-    expect(ctrl.todos.length).toEqual(0);
+    expect($scope.todoStore.length).toEqual(0);
+  });
+
+  it('user can add items to list', function() {
+    $scope.addTodo('bananas');
+    expect($scope.todoStore.length).toEqual(1);
   });
 });
