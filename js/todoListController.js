0308d095b39dc81007e8c38734cfdc3829738dfd
ana <adscarlos@gmail.com>
2015-10-25 19:23:48 +0000

    adds remove link that allows user to delete a list item

--- js/todoListController.js
+++ js/todoListController.js
@@ -5,7 +5,10 @@ app.controller('todoListController', function($scope) {
   $scope.addTodo = function() {
     $scope.todoStore.push($scope.todo);
     $scope.todo = '';
+  };
 
+  $scope.removeTodo = function(index) {
+    $scope.todoStore.splice(index, 1);
   };
 
 });
